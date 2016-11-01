680c50d942400f69855c1c70b4c33dfb27338294
helmifraser <helmifraser1@gmail.com>
2016-11-01 00:25:46 +0000

    Generate weights + encode functions

--- test.m
+++ test.m
@@ -3,7 +3,7 @@ clc
 
 %Set the input values and number of individuals in the population
 in = [2; 3];
-pop = 1;
+pop = 3;
 
 % Generate a population of size "pop", using the populate function
 population = populate(pop);
