14a4af7b84e86f01f003e420454b586eb35ba6a2
Job van Dieten <job.1994@gmail.com>
2016-11-02 15:39:58 +0000

    Attempt to evaluate neural net output with FUN

--- MY_OPTIMIZER.m
+++ MY_OPTIMIZER.m
@@ -7,6 +7,15 @@ function xbest = MY_OPTIMIZER(FUN, DIM, ftarget, maxfunevals)
   maxfunevals = min(1e8 * DIM, maxfunevals); 
   popsize = min(maxfunevals, 200);
   fbest = inf;
+  ftarget
+  in = [0.1 -1];
+%for iter=1:10
+%    weights = populate(4,2);
+%    encoded_weights = encode(weights);
+%    neural_out = neural_net_function(encoded_weights, in);
+%    fitness_value = feval(FUN, transpose(neural_out))
+%end
+
   for iter = 1:ceil(maxfunevals/popsize)
     xpop = 10 * rand(DIM, popsize) - 5;      % new solutions
     [fvalues, idx] = sort(feval(FUN, xpop)); % evaluate
@@ -14,8 +23,8 @@ function xbest = MY_OPTIMIZER(FUN, DIM, ftarget, maxfunevals)
     
     
     if fbest > fvalues(1)                    % keep best
-      fbest = fvalues(1);
-      xbest = xpop(:,1);
+     fbest = fvalues(1);
+     xbest = xpop(:,1);
     end
     if feval(FUN, 'fbest') < ftarget         % COCO-task achieved
       break;                                 % (works also for noisy functions)
--- test2.m
+++ test2.m
@@ -1,5 +1,11 @@
 clc
 clear
 
-weights = populate(4,2)
-encoded_weights = encode(weights)
+in = [0.1 -1];
+for iter=1:10
+    weights = populate(4,2);
+    encoded_weights = encode(weights);
+    neural_out = neural_net_function(encoded_weights, in);
+    fitness_value = feval('fgeneric', transpose(neural_out))
+end
+
