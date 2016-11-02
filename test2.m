02087a6e4e8fe14b53cface64ca409ce98b62f0f
helmifraser <helmifraser1@gmail.com>
2016-11-02 11:37:39 +0000

    Populate and encode
    Can now create a population of weights and extract their value

---
+++ test2.m
@@ -0,0 +1,5 @@
+clc
+clear
+
+weights = populate(4,50)
+encoded_weights = encode(weights)
---
+++ tournament.m
@@ -0,0 +1,5 @@
+% Selects k parents to fight for the right to reproduce
+
+function s = tournament(fitness, k)
+    
+end
