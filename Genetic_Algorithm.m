154424f41bde4f6b9a13848e1f1f5d7268f12077
Job van Dieten <job.1994@gmail.com>
2016-11-04 15:28:23 +0000

    Final Tournament selection Commit

--- Genetic_Algorithm.m
+++ Genetic_Algorithm.m
@@ -2,15 +2,16 @@ function weights_best = GA(io_pairs, n_pairs, DIM)
 
 pop_size = 10;
 lim_generations = 10;
+k_parents = 5;
 
-%first generation
+%create first generation
 pop = populate(pop_size, DIM);
 weights = encode(pop);
 
-f_array = evaluate_generation(pop_size, weights, n_pairs, io_pairs);
+generation_fitness = evaluate_generation(pop_size, weights, n_pairs, io_pairs);
 
-%tournament selection
-
+%tournament selection for mutation/crossover
+next_generation = tournament(generation_fitness, pop, k_parents, pop_size);
 %produce next generation
 
 
--- tournament.m
+++ tournament.m
@@ -1,5 +1,34 @@
 % Selects k parents to fight for the right to reproduce
 
-function s = tournament(fitness, k)
+function next_generation = tournament(fitness_array, pop, k_parents, popsize)
+    next_generation = zeros(1, popsize);
+    for idx = 1 : popsize
+        select = randperm(popsize, k_parents);
     
+        for i = 1 : k_parents
+        check(1, i) = fitness_array(select(i)); 
+        end
+    
+        f_parent1 = inf;
+        f_parent2 = inf;
+    
+        %get two genes with the best fitnesses
+        for i = 1 : k_parents
+            if(fitness_array(select(i)) < f_parent1)
+               if(f_parent1 < f_parent2)
+                  f_parent2 = f_parent1; 
+               end
+           
+            f_parent1 = fitness_array(select(i));
+           
+            elseif(fitness_array(select(i)) < f_parent2)
+                f_parent2 = fitness_array(select(i));
+            end
+        end
+    
+        parent1 = pop(find(fitness_array==f_parent1),:);
+        parent2 = pop(find(fitness_array==f_parent2),:);
+        
+        next_generation(1, idx) = child;
+    end
 end
