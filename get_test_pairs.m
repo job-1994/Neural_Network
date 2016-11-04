6a2f320540923ec54b38e1057a872d7574291152
helmifraser <helmifraser1@gmail.com>
2016-11-04 15:30:54 +0000

    Mutate fix + crossover

--- get_test_pairs.m
+++ get_test_pairs.m
@@ -1,12 +1,18 @@
 
+function pairs = get_test_pairs(n_pairs, DIM, FUN)
+    inputs = cell(1,n_pairs);
+    outputs = zeros(1,n_pairs);
+    pairs = cell(1,2);
+    
+    for i = 1:n_pairs
+        input =  -5 + (5+5)*rand(1, DIM);
+        inputs{1,i} = transpose(input);
+    end
 
-function p = get_test_pairs(N)
-    p = zeros(N,2);
-    count = -1*((N-1)/2);
-    for i = 1:N
-        p(i,1) = count;
-        p(i,2) = 0;
-        count = count + 1;
+    for j = 1:n_pairs
+        outputs(1,j) = feval(FUN, inputs{1,j});
     end
     
+    pairs{1,1} = inputs;
+    pairs{1,2} = outputs;
 end
