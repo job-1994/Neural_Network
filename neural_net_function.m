fb92de718c758f2c74b863f07143a676cf03e9a3
Job van Dieten <job.1994@gmail.com>
2016-11-09 19:46:17 +0000

    changed activation function constants and added comments to neural net function, tested in test 2

--- neural_net_function.m
+++ neural_net_function.m
@@ -1,25 +1,29 @@
-function Output = neural_net_function(input, Weights, nHiddenLayers)
+%This function provides the Neural Network Calculations, it takes in the
+%matrix of inputs (size depends on dimensions), the weights for the neural
+%network organized by encode(), and the amount of hidden layers.
+
 %Activation function per layer is a*tanh(b*previous_layer_output), the last
 %layers output is multiplied by a final weight that determines the
 %magnitude of the output value, as this differs per function.
-    a = 2;
-    b = 1;
+function Output = neural_net_function(input, Weights, nHiddenLayers)
+    a = 1;
+    b = 2;
     
     %Input layer
     input = transpose(input);
     input_weight_layer = Weights{1};
-    layer_output = tanh(input * input_weight_layer);
+    layer_output = a*tanh(b*input * input_weight_layer);
     
     %Cycles through hidden layers, as defined in the passed in parameters
     for layer = 1: nHiddenLayers
         weight_layer = transpose(Weights{2}{layer});
-        layer_output = b*tanh(a*layer_output * weight_layer);
+        layer_output = a*tanh(b*layer_output * weight_layer);
     end
     
-    %Output layer, lastly multiplied by weight containing order of
+    %Output layer, lastly multiplied by weight containing the order of
     %magnitude
     output_weight_layer = transpose(Weights{3});
-    layer_output = b*tanh(a*layer_output * output_weight_layer);
+    layer_output = a*tanh(b*layer_output * output_weight_layer);
     Output = Weights{4}*(layer_output);
 
 end
--- test2.m
+++ test2.m
@@ -1,16 +1,30 @@
 clc
 clear
-% N = 2;
-% dimensions = 2;
-% nHiddenLayers = 3;
-% neurons = 2;
-% input = [2; 2];
-% test = populate(N, dimensions, nHiddenLayers, neurons)
-% tested = encode(test, nHiddenLayers, neurons, dimensions)
-% weight = {tested{1, 1} tested{1, 2} tested{1, 3} tested{1, 4}};
-% 
-% neural_net = neural_net_function(input, weight, nHiddenLayers);
-% display(test);
+N = 2;
+dimensions = 2;
+nHiddenLayers = 2;
+neurons = 2;
+input = -5 + (5 + 5)*rand(dimensions,1);
+test = populate(N, dimensions, nHiddenLayers, neurons);
+tested = encode(test, nHiddenLayers, neurons, dimensions);
+weight = {tested{1, 1} tested{1, 2} tested{1, 3} tested{1, 4}};
 
-y = tanh(2.5)
+a = 1;
+b = 2;
+    
+input = transpose(input);
+input_weight_layer = weight{1};
+layer_output = a*tanh(b*input * input_weight_layer);
+    
+for layer = 1: nHiddenLayers
+    weight_layer = transpose(weight{2}{layer});
+    layer_output = a*tanh(b*layer_output * weight_layer);
+end
+
+output_weight_layer = transpose(weight{3});
+layer_output = a*tanh(b*layer_output * output_weight_layer);
+% Output = weight{4}*(layer_output)
+Output = 100*(layer_output)
+
+
 
